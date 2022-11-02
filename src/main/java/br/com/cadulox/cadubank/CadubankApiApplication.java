231ce7a1b33d1a48c50ac2e22ea9f71a5b2db4c4
Cadulox <engenharia.lourenco@gmail.com>
2022-11-02 09:46:49 -0300

    feat: create project

---
+++ src/main/java/br/com/cadulox/cadubank/CadubankApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.cadulox.cadubank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadubankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadubankApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cadulox/cadubank/CadubankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cadulox.cadubank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadubankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
